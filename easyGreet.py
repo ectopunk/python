f28054c3c6af5a871c2c597019d54d6919f48536
ectopunk <ectopunk@multiverse.org>
2017-08-29 16:08:54 -0700

    more testing and learning and making my first function.

---
+++ easyGreet.py
@@ -0,0 +1,9 @@
+while True:
+  print('Enter a number to get a greeting.')
+  spam = input()
+  if spam == '1':
+    print('Hello')
+  elif spam == '2':
+    print('Howdy')
+  else:
+    print('Greetings!')
---
+++ helloFunc.py
@@ -0,0 +1,7 @@
+def hello():
+	print('Howdy!')
+	print('Greetings!')
+	print('Hello there.')
+
+hello()
+hello()
